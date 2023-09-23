b5ab37e46d2c632c38e0e99c26c0db5c17b23ca7
immediatealex <alex.nicholas@immediate.co.uk>
2023-09-23 11:12:34 +0100

    test deployment

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,20 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "netlify",
+  publicPath: "/build/",
+  serverBuildPath: ".netlify/functions-internal/server.js",
+  serverConditions: ["deno", "worker"],
+  serverMainFields: ["main", "module"],
+  serverModuleFormat: "cjs",
+  serverPlatform: "node",
+  serverMinify: false,
   server:
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
       ? "./server.js"
       : undefined,
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: ".netlify/functions-internal/server.js",
-  // publicPath: "/build/",
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  future: {
+    v2_dev: true,
+  },
 };
