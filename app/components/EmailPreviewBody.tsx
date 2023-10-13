413a3dd76bc7b7e331b5e23a5b36f548caa088fd
Alex Nicholas <100680756+immediatealex@users.noreply.github.com>
2023-10-13 11:03:30 +0100

    fix: open iframe links in new tab (#26)
    * fix: open iframe links in new tab
    
    * chore: eof line

--- app/components/EmailPreviewBody.tsx
+++ app/components/EmailPreviewBody.tsx
@@ -1,11 +1,31 @@
 import clsx from 'clsx';
 import type { Email } from '@prisma/client';
+import * as React from 'react';
 
 export function EmailPreviewBody({ html }: { html?: Email['html'] }) {
+  const htmlWithBaseTag = React.useMemo(() => {
+    if (!html) return '';
+
+    // This ensures links open in a new tab and not in the iframe
+    const baseTag = '<base target="_blank">';
+    const headTag = '<head>';
+    const headTagIndex = html.indexOf(headTag);
+
+    if (headTagIndex === -1) {
+      return html;
+    }
+
+    return (
+      html.slice(0, headTagIndex + headTag.length) +
+      baseTag +
+      html.slice(headTagIndex + headTag.length)
+    );
+  }, [html]);
+
   return (
     <iframe
       className={clsx('email-preview', 'h-full w-full')}
-      srcDoc={html}
+      srcDoc={htmlWithBaseTag}
       title="Email preview"
     />
   );
