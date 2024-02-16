27cd88c726754df76b4d7f1793b64c6756c8bc46
immediatealex <alex.nicholas@immediate.co.uk>
2024-02-16 15:26:18 +0000

    fix: filter out pino-pretty in production

---
+++ app/lib/logger.server.ts
@@ -0,0 +1,22 @@
+import pino from 'pino';
+
+const targets: pino.TransportMultiOptions['targets'] = [
+  {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  },
+  {
+    target: '@axiomhq/pino',
+    options: {
+      dataset: process.env['AXIOM_DATASET'],
+      token: process.env['AXIOM_TOKEN'],
+    },
+  },
+].filter(
+  ({ target }) =>
+    process.env.NODE_ENV !== 'development' && target !== 'pino-pretty'
+);
+
+export const logger = pino({ level: 'info' }, pino.transport({ targets }));
